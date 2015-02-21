305db29850b28596f174b2358a51d0de005caa6b
Alexei Klika <AlexeiKlika@users.noreply.github.com>
2015-02-21 15:56:29 -0600

    Start lesson 1 exercises, and add some practice JS to the js/resumeBuilder.js file.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,4 @@
+    var awesomeThoughts = 'My name is Alexei and I am AWESOME!';
+    console.log(awesomeThoughts);
+    var funThoughts = awesomeThoughts.replace('AWESOME','FUN');
+    $('#main').append(funThoughts);
